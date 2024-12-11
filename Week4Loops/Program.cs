441f61909a95a10a0758162368498449b59c0147
Rainer K. <rainer.koiker@tktk.ee>
2024-12-11 22:28:52 +0200

    add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿//FOR-LOOP
+
+
+//DRY-principle - Do Not Repeat yourself
+
+int i = 0;
+
+for(i = 0; i < 5; i++)
+{
+    Console.WriteLine("We dont need no education.");
+}
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day");
